9b7f1d437060c8ae442814b7100e30952d1c779a
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2023-04-03 09:03:55 -0400

    update-automated-save

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # go-argo-cron
+
+
+```bash
+go get k8s.io/client-go@v0.26.3
+```
---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	v1alpha1 "github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
+	argo "github.com/argoproj/argo-workflows/v3/pkg/client/clientset/versioned"
+	apiv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
+)
+
+func getClientset() (*argo.Clientset, error) {
+	var config *rest.Config
+	var err error
+	// Check if the program is running inside a Kubernetes cluster.
+	if _, err = rest.InClusterConfig(); err != nil {
+		kubeconfig := os.Getenv("KUBECONFIG")
+		if kubeconfig == "" {
+			kubeconfig = clientcmd.RecommendedHomeFile
+		}
+
+		config, err = clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+			&clientcmd.ClientConfigLoadingRules{ExplicitPath: kubeconfig},
+			&clientcmd.ConfigOverrides{ClusterInfo: clientcmdapi.Cluster{Server: ""}}).ClientConfig()
+		if err != nil {
+			log.Fatalf("Failed to load Kubernetes configuration: %v", err)
+		}
+	} else {
+		config, err = rest.InClusterConfig()
+		if err != nil {
+			log.Fatalf("Failed to load in-cluster configuration: %v", err)
+		}
+	}
+
+	//return kubernetes.NewForConfig(config)
+	return argo.NewForConfig(config)
+
+}
+
+func main() {
+
+	argoClientset, err := getClientset()
+	if err != nil {
+		log.Fatalf("Error creating Argo clientset: %v", err)
+	}
+
+	ctx := context.Background()
+
+	cronWorkflow := &v1alpha1.CronWorkflow{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: "hello-world-cron-",
+		},
+		Spec: v1alpha1.CronWorkflowSpec{
+			Schedule:          "* * * * *", // Run every minute
+			ConcurrencyPolicy: "Forbid",
+			WorkflowSpec: v1alpha1.WorkflowSpec{
+				Entrypoint: "hello-world",
+				Templates: []v1alpha1.Template{
+					{
+						Name: "hello-world",
+						Script: &v1alpha1.ScriptTemplate{
+							Container: apiv1.Container{
+								Image:   "python:alpine3.6",
+								Command: []string{"python"},
+							},
+							Source: "print('Hello, world!')",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	_, err = argoClientset.ArgoprojV1alpha1().CronWorkflows("default").Create(ctx, cronWorkflow, metav1.CreateOptions{})
+	if err != nil {
+		log.Fatalf("Error creating Argo Cron Workflow: %v", err)
+	}
+
+	fmt.Println("Cron Workflow created successfully.")
+
+	for {
+		time.Sleep(30 * time.Second)
+		workflows, err := argoClientset.ArgoprojV1alpha1().Workflows("default").List(ctx, metav1.ListOptions{})
+		if err != nil {
+			log.Printf("Error listing Argo Workflows: %v", err)
+			continue
+		}
+
+		fmt.Println("Workflows:")
+		for _, wf := range workflows.Items {
+			fmt.Printf(" - Name: %s, Status: %s\n", wf.Name, wf.Status.Phase)
+		}
+	}
+}
